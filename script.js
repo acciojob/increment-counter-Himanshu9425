8626d924401267f35163277a26491adefa784a37
Himanshu9425 <147365994+Himanshu9425@users.noreply.github.com>
2025-02-19 16:42:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+function Increment() {
+	let count = document.querySelector("#ptag");
+	let counter=parseInt(count.textContent);
+    counter+=1;
+    count.innerHTML=`
+    <p>${counter}<p>
+    `
+
+}
